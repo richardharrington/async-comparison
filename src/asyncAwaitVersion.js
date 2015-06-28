3af44e20cbbbd35f12509f2d238bbd71694b28d6
Richard Harrington <harrington.richard@gmail.com>
2015-06-28 01:23:50 -0400

    async/await almost working

---
+++ src/asyncAwaitVersion.js
@@ -0,0 +1,49 @@
+import { randomWordEndpoint, movieSearchEndpoint, movieEndpoint } from 'endpoints';
+
+function get(url) {
+  return new Promise(resolve => {
+    const req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.onload = () => {
+      const response = JSON.parse(req.response);
+      resolve(response);
+    };
+    req.send();
+  });
+}
+
+async function getParallel(urls) {
+  return Promise.all(urls.map(get));
+  //return await urls.map(get);
+}
+
+async function fetchRandomWord() {
+  const [{word}] = await get(randomWordEndpoint);
+  return word;
+}
+
+async function fetchMovieStubsFromSearch(word) {
+  const url = movieSearchEndpoint + encodeURIComponent(word);
+  const {Search: movieStubs} = await get(url);
+  return movieStubs;
+}
+
+async function fetchMovies(movieStubs) {
+  const urls = movieStubs.map(movieStub => movieEndpoint + movieStub.imdbID);
+  return await getParallel(urls);
+}
+
+async function launchMovieSearch() {
+  const word = await fetchRandomWord();
+  const movieStubs = await fetchMovieStubsFromSearch(word);
+  if (movieStubs) {
+    const movies = await fetchMovies(movieStubs);
+    const finalResults = {word, movies};
+    return finalResults;
+  }
+  else {
+    return await launchMovieSearch();
+  }
+}
+
+export default { launchMovieSearch };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import callbacksVersion from 'callbacksVersion';
 import promisesVersion from 'promisesVersion';
 import cspVersion from 'cspVersion';
 import coVersion from 'coVersion';
+import asyncAwaitVersion from 'asyncAwaitVersion';
 
 const log = console.log.bind(console);
 
@@ -15,4 +16,15 @@ csp.go(function*() {
 });
 co(function*() {
   log("co:", yield coVersion.launchMovieSearch());
-})
+});
+(async function() {
+  log("async/await:", await asyncAwaitVersion.launchMovieSearch());
+})();
+
+// Something's wrong with this. Async/await seems to be working
+// but is returning promises, not movies.
+
+// ok, working now but had to put Promise.all in getParallel.
+// is there a better way using async/await?
+
+
